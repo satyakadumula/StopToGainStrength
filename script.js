b2f7f1bac365296d94b785b0b2810e2651c64bb9
satya kadumula <kadumulasatya@gmail.com>
2024-12-03 08:31:54 +0530

    Styling is Done

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const listItems = [
-    "bdcbajbv am va",
-    "jsbdcajvdabcajbbhjb",
-    "bsdvfusv d dvavasdv",
-    "sjdvbabvakbvkabva",
-    "oiisuiyuFGJWVDEF ",
-    "qlkwjehifahksbjvdjb",1,2,3,3,4,4,5,5,6,7,7,90
+    "bdcbajbv am vajdscxscvrtfh xzXCWREre scrdvbfngh jhugy ftrdrX$ ztyst dy5r6tntfr ",
+    "jsbdcajvdabcajbbhjb jsbdcajvd  abcajbbh jbjsb dcajvdabc ajbbhjbj sbdcajvda bcajbbh jbjsb dcajvdabc ajbbhjb",
+    "bsdvfusv d dvavasdv jsbdcajv dabcajbb hjbjs  bdcajvda bcajb bhjbjsb dcajvdab cajbbhj bjsbdc ajvdabca  jbbhjb",
+    "sjdvba bvakbv kabvajsbd cajvdabcaj bbhj bjsbdcajv dabcaj bbhjb",
+    "oiisuiyu FGJWVDEF jsbdcajvd abcajbbh  jbjsb dcajvdab cajbbh jbjsbd cajvdabc ajbbhjbj sbdcajvd abcajbbh jbjsb dcajvd abcajbb hjb jsbdcaj vdabcaj bbhjb ",
+    "qlkwjeh ifahksbjvdjbj sbdcajvdab  cajbbhjbj  sbdcajv dabcaj bbhjbjs  bdcajv dabca  jbbhjb",1,2,3,3,4,4,5,5,6,7,7,90
 ];
 
 let quote = document.querySelector("#quote");
@@ -31,4 +31,4 @@ btn.addEventListener('click', function() {
 });
 
 
-iterator();
+// iterator();
